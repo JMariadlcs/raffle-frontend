a102899c15879c16557074a438968856ed81e92a
JMaria <jmariadelacruzsanchez@gmail.com>
2022-06-07 16:52:03 +0200

    update

--- components/ManualHeader.jsx
+++ components/ManualHeader.jsx
@@ -52,7 +52,7 @@ export default function ManualHeader() {
                                     // window.localStorage.setItem("connected", "walletconnect")
                                 }
                             }}
-                            disabled={isWeb3EnableLoading}
+                            disabled={isWeb3EnableLoading} // disable connect button if it is loading
                             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-auto"
                         >
                             Connect
